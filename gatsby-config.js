7f6667b64720a4b67fafeb50e3bd0dfa6c5d6480
Jake-Leicht <JakeSnakeBCU@gmail.com>
2023-02-08 23:54:03 -0600

    adding gh-pages update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/gatsby-budget-app",
   siteMetadata: {
     title: `gatsby-budget-app`,
     siteUrl: `https://www.yourdomain.tld`
